e0da789c78548711c0c24cab0ddc247380b57cc4
Markus Ilmola <markus.ilmola@pp.inet.fi>
2016-03-27 20:57:38 +0300

    Initial import.

--- README.md
+++ README.md
@@ -1,2 +1,136 @@
-# Try
-An exceptionally simple test framework for C++11 and later
+# Try - An exceptionally simple test framework for C++11 and later
+
+This is a header only library. There is only one header file and it's only a
+couple of hundreds lines of code. This library has only two classes, no free 
+functions and only one tiny tiny macro.
+
+To use this library include `Try.hpp`.
+
+~~~c++
+#include <Try/Try.hpp>
+~~~
+
+First create a instance of `Try` (from name space `Try`).
+
+~~~c++
+Try::Try tr{};
+~~~
+
+You can optionally give a log stream. The default stream is 'std::cout'.
+
+~~~c++
+std::stringstream log{};
+Try tr{log};
+~~~
+
+To run a test case use the `()`-operator.
+
+~~~c++
+template <typename F, typename... Args>
+bool Try::operator()(SourceContext&& sc, F test, const Args&... args) noexcept;
+~~~
+
+To create a `SourceContext` use the macro `SC`. It is the *only* macro needed.
+It creates a `SourceContext` so if a test case fails the line can be reported.
+
+The parameter `test` is the test to run. Usually a lambda function. The 
+return value of the test case is ignored. *To fail the test throw any exception*
+with an appropriate message. 
+
+`args` are the arguments forwarded to the test case when called. You can 
+alternatively use lambda capture to pass arguments to the test case.
+
+The return value is `true` if the test succeeds. If the test fails, the return 
+value is `false` and the error message from the exception and the arguments are 
+is logged to the stream. You can log additional messages to the log by calling 
+`Try::os()` to get the stream.
+
+~~~c++
+Try::Try tr{};
+
+tr.os() << "Testing mySquare().\n";
+
+tr(SC, [] (int in, int out) {
+	int result = mySquare(in);
+	if (result != out) 
+		throw std::runtime_error("Invalid result: "+std::to_string(result));
+}, -2, 4);
+~~~
+
+Possible output:
+
+~~~console
+Testing mySquare().
+Test failed: foo.cpp, line 14
+Message: "Invalid result: -4"
+Arguments:
+"-2" (i)
+"4" (i)
+~~~
+
+Another examble:
+
+~~~c++
+tr(SC, [] (std::size_t index, int value) {
+	std::vector<int> data{1,2,3};
+	data.at(index) = value;
+ }, 4, 10);
+~~~
+
+Possible output:
+
+~~~console
+Test failed: foobar.cpp, line 12
+Message: "vector::_M_range_check"
+Arguments:
+"4" (i)
+"10" (i)
+~~~
+
+As testing for equality in the most common test case, there is a helper method 
+`equal` to construct a test case for it.
+
+~~~c++
+template <typename T1, typename T2>
+void Try::equal(SourceContext&& sc, const T1& a, const T2& b) noexcept;
+~~~
+
+Example:
+
+~~~c++
+std::string foobar("foobar");
+tr.equal(SC, foobar.size(), 6);
+~~~
+
+Similarly there are also methods `notequal`, `less`, and `lequal`.
+
+Another common case is to test if a function throws on invalid input. Helper
+method `throws` will construct a test case that will fail if the test case does 
+not throw.
+
+~~~c++
+template <typename F, typename... Args>
+void Try::throws(SourceContext&& sc, F test, const Args&... args) noexcept;
+~~~
+
+Example:
+
+~~~c++
+tr.throws(SC, [] () {
+	std::vector<int> data{1, 2, 3};
+	data.at(data.size());
+});
+~~~
+
+To nest tests capture the Try instance and use it inside another test.
+
+~~~c++
+tr(SC, [&tr] () {
+	tr.os() << "test\n";
+	tr(SC, [&tr] () {
+		tr.os() << "subtest\n";
+	});
+});
+~~~
+
+
---
+++ include/Try/Try.hpp
@@ -0,0 +1,220 @@
+#ifndef UUID_E65B69577BCE470086F19A6104A73D71
+#define UUID_E65B69577BCE470086F19A6104A73D71
+
+#include <cstdint>
+#include <iostream>
+#include <type_traits>
+#include <stdexcept>
+#include <typeinfo>
+
+
+
+namespace Try {
+
+
+/**
+ * Helper class for tests to capture the source context.
+ * Use the macro SC to create a SourceContext.
+ */
+class SourceContext {
+public:
+
+	SourceContext(const char* file, std::size_t line) noexcept :
+		file(file), line(line) { }
+
+	const char* file;
+
+	std::size_t line;
+
+};
+
+#define SC ::Try::SourceContext{__FILE__, __LINE__}
+
+inline std::ostream& operator<<(std::ostream& os, const SourceContext& sc) noexcept {
+	os << sc.file << ", line " << sc.line;
+	return os;
+}
+
+
+namespace detail {
+
+
+template<typename T>
+class is_streamable {
+public:
+	template <typename U>
+	static auto test(int) -> decltype(
+		std::declval<std::ostream&>() << std::declval<U>(), std::true_type()
+	);
+
+	template<typename>
+	static auto test(...) -> std::false_type;
+
+	static constexpr bool value =
+		std::is_same<decltype(test<T>(0)), std::true_type>::value;
+};
+
+
+template <typename T, typename std::enable_if<is_streamable<T>::value>::type* = nullptr>
+void stream(std::ostream& os, const T& arg) {
+	os << "\"" << arg << "\" (" << typeid(T).name() << ")";
+}
+
+template <typename T, typename std::enable_if<!is_streamable<T>::value>::type* = nullptr>
+void stream(std::ostream& os, const T&) {
+	os << "[Can't print'] (" << typeid(T).name() << ")";
+}
+
+inline void streamArgs(std::ostream& os) noexcept { os << std::endl; }
+
+template <typename T, typename... Args>
+void streamArgs(std::ostream& os, const T& first, const Args&... args) noexcept {
+	stream(os, first);
+	os << std::endl;
+	streamArgs(os, args...);
+}
+
+}
+
+
+/**
+ * A class for running test cases.
+ * Use the ()-operator to run a test case. To fail the test throw any exception.
+ */
+class Try {
+public:
+
+	/// @param os A stream to log test errors to.
+	explicit Try(std::ostream& os = std::cout) noexcept :
+		mOs{&os},
+		mSuccesCount{0},
+		mFailCount{0}
+	{ }
+
+	/// @return The stream used for logging test errors.
+	/// Can be used to log additional messages.
+	std::ostream& os() const noexcept { return *mOs; }
+
+	/// Runs the given test case with the given arguments. The test must throw
+	/// on failure. The return value is ignored. The message from the exception
+	/// along with the arguments given to the test are logged to the stream.
+	/// @param sc Use the macro SC in place of this parameter.
+	/// @param test A lambda function that performs the test.
+	/// @param args Given to the test case function when called.
+	/// @return false if test fails (throws) and true otherwise
+	template <typename F, typename... Args>
+	bool operator()(SourceContext&& sc, F test, const Args&... args) noexcept {
+		try {
+			test(args...);
+			++mSuccesCount;
+			return true;
+		}
+		catch (std::exception& e) {
+			os() << "Test failed: " << sc << std::endl;
+			os() << "Message: \"" <<  e.what() << "\"" << std::endl;
+		}
+		catch (...) {
+			os() << "Test failed: " << sc << std::endl;
+			os() << "(no message)" << std::endl;
+		}
+
+		if (sizeof...(args) == 0) {
+			os() << "(no arguments)" << std::endl;
+		}
+		else {
+			os() << "Arguments:" << std::endl;
+			detail::streamArgs(os(), args...);
+		}
+
+		++mFailCount;
+		return false;
+	}
+
+	/// Constructs and runs a test case that checks if a == b.
+	/// The test will succeed if a == b and fails if not or if the "==" throws.
+	/// @param sc Use the macro SC in place of this parameter.
+	/// @param a,b Values to compare with the == operator.
+	/// @return true if the test succeeds false if not.
+	template <typename T1, typename T2>
+	bool equal(SourceContext&& sc, const T1& a, const T2& b) noexcept {
+		return (*this)(std::move(sc), [](const T1& a, const T2& b){
+			if (!(a == b)) throw std::runtime_error("Arguments are not equal!");
+		}, a, b);
+	}
+
+	/// Constructs and runs a test case that checks if a != b.
+	/// The test will succeed if a != b and fail if not or if "!=" throws.
+	/// @param sc Use the macro SC in place of this parameter.
+	/// @param a,b Values to compare with the != operator.
+	/// @return true if the test succeeds false if not.
+	template <typename T1, typename T2>
+	bool notequal(SourceContext&& sc, const T1& a, const T2& b) noexcept {
+		return (*this)(std::move(sc), [](const T1& a, const T2& b){
+			if (!(a != b)) throw std::runtime_error("Arguments are equal!");
+		}, a, b);
+	}
+
+	/// Constructs and runs a test case that checks if a < b.
+	/// The test will succeed if a < b and fails if not or if "<" throws.
+	/// @param sc Use the macro SC in place of this parameter.
+	/// @param a,b Values to compare with the < operator.
+	/// @return true if the test succeeds false if not.
+	template <typename T1, typename T2>
+	bool less(SourceContext&& sc, const T1& a, const T2& b) noexcept {
+		return (*this)(std::move(sc), [](const T1& a, const T2& b){
+			if (!(a < b)) throw std::runtime_error("The first argument is not less than the second!");
+		}, a, b);
+	}
+
+	/// Constructs and runs a test case that checks if a <= b.
+	/// The test will succeed if a <= b and fails if not or if "<=" throws.
+	/// @param sc Use the macro SC in place of this parameter.
+	/// @param a,b Values to compare with the <= operator.
+	/// @return true if the test succeeds false if not.
+	template <typename T1, typename T2>
+	bool lequal(SourceContext&& sc, const T1& a, const T2& b) noexcept {
+		return (*this)(std::move(sc), [](const T1& a, const T2& b){
+			if (!(a <= b)) throw std::runtime_error("The first argument is not less than or equal to the second!");
+		}, a, b);
+	}
+
+	/// Constructs and runs a test case that succeeds if it throws and fails if
+	/// it does not. The return value will make no difference.
+	/// @param sc Use the macro SC in place of this parameter.
+	/// @param test A lambda function that runs the test.
+	/// @param args The arguments given to the lambda when run.
+	/// @return true if the test succeeds and false if it fails.
+	template <typename F, typename... Args>
+	bool throws(SourceContext&& sc, F test, const Args&... args) noexcept {
+		return (*this)(std::move(sc), [test](const Args&... args) {
+			try {
+				test(args...);
+			}
+			catch (...) {
+				return;
+			}
+			throw std::runtime_error("Test did not throw!");
+		}, args...);
+	}
+
+	/// @return The number of successful tests run.
+	std::size_t succesCount() const noexcept { return mSuccesCount; }
+
+	/// @return The number of failed tests run.
+	std::size_t failCount() const noexcept { return mFailCount; }
+
+private:
+
+	std::ostream* mOs;
+
+	std::size_t mSuccesCount;
+
+	std::size_t mFailCount;
+
+};
+
+
+}
+
+
+#endif
